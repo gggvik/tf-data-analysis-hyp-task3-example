c1ca217461cda9947836a138db5925855573034e
hhhhh <52508643+gggvik@users.noreply.github.com>
2023-04-13 16:20:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 250483508 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(sample: np.ndarray) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+       alpha = 0.04
+    _, p_value = ttest_1samp(sample, 300)
+
+    if p_value < alpha:
+        return True
+    else:
+        return False
